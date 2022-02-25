1480dee740ade0d2341f424f8187cc01f7f18474
admin <admin@Mac-mini-de-admin.local>
2022-02-25 10:07:37 +0100

    feat: added the first project action button

--- tp1/tp1/ViewController.swift
+++ tp1/tp1/ViewController.swift
@@ -13,7 +13,30 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    // deisgne un element d'interface graphique
+    @IBOutlet weak var textTitre: UILabel!;
+    // weak dans le type de var veux dire que la gestion mémoire de la variable est faite ailleurs
+    @IBOutlet weak var zoneSaisie: UITextField!; // "!" car la var n'est pas encore créé
 
+    // deisgne une methode associé à un événement graphique
+    @IBAction func boutonLancer(_ sender : UIButton){
+        
+    }
+    
+    @IBAction func boutonCacher(_ sender : UIButton){
+        if sender.currentTitle == "Cacher" {
+            textTitre.isHidden = true
+            zoneSaisie.isHidden = true
+            sender.setTitle("Montrer", for: .normal)
+        }else{
+            textTitre.isHidden = false
+            zoneSaisie.isHidden = false
+            sender.setTitle("Cacher", for: .normal)
+        }
+    }
+        
+    
 
 }
 
